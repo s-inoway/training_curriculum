d0ab200facd401621fc02ed5aa9cc8512119c38f
s-inoway <s.inoway.0804.kitchey@gmail.com>
2020-12-01 21:47:12 +0900

    レビュー内容の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -42,6 +41,5 @@ class CalendarsController < ApplicationController
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
-
   end
 end
